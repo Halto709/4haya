cbd4c1c9b99b7f578a0624385aef6ddcb0a58469
Halto709 <e1b22071@oit.ac.jp>
2024-10-29 15:37:29 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/quizknockn/yonhaya/YonhayaApplication.java
@@ -0,0 +1,13 @@
+package oit.is.quizknockn.yonhaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YonhayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YonhayaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/quizknockn/yonhaya/YonhayaApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.quizknockn.yonhaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YonhayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
